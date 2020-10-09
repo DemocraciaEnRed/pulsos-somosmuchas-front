2a104c2f1a8acb9cf301025dbf62ddc42ef90230
franz <franzisca@disroot.org>
2020-10-09 15:17:40 -0300

    Cambio de link/url de api de back

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 const packageVars = require('../../package.json');
 
 export const environment = {
-  api: 'https://backend.activa.causascomunes.org/',
-  imgBase: 'https://backend.activa.causascomunes.org/',
+  api: 'https://adminasm.democraciaenred.org/',
+  imgBase: 'https://adminasm.democraciaenred.org/',
   production: true
 };
